7090fc3777b0bd45f2a61c082c348bfb00e755fd
haya <91950797+nonosaku@users.noreply.github.com>
2021-12-31 15:22:49 +0900

    Update silly-story-generator.js

--- assessment-finished/silly-story-generator.js
+++ assessment-finished/silly-story-generator.js
@@ -1,1 +1,44 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+const storyText = 'その日はとても有意義だった。:insertx:は飛び立った。:inserty:に到着した, しかし雨が降ってきた。 :insertz:は晴れの王と喧嘩中だった。 :insertx: は羽をびしょ濡れにされ、天気の王には負けることはいつもの話であった。';
+const insertX = ['悪魔', '天使', '神'];
+const insertY = ['人間界', '天界', '雲'];
+const insertZ = ['雨の王', '雷の王', '風の王'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(':insertx:',xItem);
+  newStory = newStory.replace(':insertx:',xItem);
+  newStory = newStory.replace(':inserty:',yItem);
+  newStory = newStory.replace(':insertz:',zItem);
+
+  if (customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replace('Bob', name);
+  }
+
+  if (document.getElementById("uk").checked) {
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
 
